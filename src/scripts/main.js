4541bf6a9e887e760c53e0a325cfbf7992e46d67
Matheus Barbosa <matts.barbosa@gmail.com>
2023-05-09 16:25:15 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
+        evento.preventDefault();
+        const numeroMaximo = parseInt(document.getElementById('numero-maximo').value);
+        
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        
+        document.getElementById('resultado-valor').innerText = Math.floor(numeroAleatorio + 1);
+        document.querySelector('.resultado').getElementsByClassName.style.display = 'block';
+    })
+})
